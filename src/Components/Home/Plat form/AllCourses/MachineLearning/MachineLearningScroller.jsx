1abaf234773d0de4bb3939113e6c186c5927b84c
Ashwini <ashwini.swe@gmail.com>
2024-05-10 15:21:25 +0530

    updated code

--- src/Components/Home/Plat form/AllCourses/MachineLearning/MachineLearningScroller.jsx
+++ src/Components/Home/Plat form/AllCourses/MachineLearning/MachineLearningScroller.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const FullStackCard = () => {
   return (
     <>
-      <div class="h-screen w-full flex items-center justify-center overflow-hidden ">
+      <div class="md:h-[500px] h-screen w-full flex items-center justify-center overflow-hidden ">
         <div class="slider relative w-[290px] h-[290px] [transform-style:preserve-3d] animate-rotate">
           <span
             style={{ "--i": 1 }}
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://media.dev.to/cdn-cgi/image/width=1080,height=1080,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fqn59agr1g40svv6krfkz.jpg"
+              src="https://etimg.etb2bimg.com/photo/100088163.cms"
               alt="img - 1"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
@@ -20,8 +20,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://media.dev.to/cdn-cgi/image/width=1080,height=1080,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fi%2Fyhr84hde9pnkd0y42plu.png"
-              alt="img - 2"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyu82toF-zpP88tb2SdATND1_B4GYlK1PME7tmhlVmA&s"
+              alt="img - 2 ML"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
@@ -30,8 +30,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://media.dev.to/cdn-cgi/image/width=1080,height=1080,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fi%2Faj976wx7kjm0m5lkfrui.png"
-              alt="img - 3"
+              src="https://imageio.forbes.com/specials-images/imageserve/648aaa9fac6d92c810b12f50/Why-Companies-Are-Vastly-Underprepared-For-The-Risks-Posed-By-AI/960x0.jpg?height=406&width=711&fit=bounds"
+              alt="img - 3 AI"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
@@ -40,8 +40,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://play-lh.googleusercontent.com/cyTI43JCjc4L-e1m7FvJhI1VhmTSJ4AMNZoqhkp0Xm6_NOtHbaewp9UPVLw5R3-tZIU"
-              alt="img - 4"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyPzjUu62iL_jxa6WtG3crCpNYmXtuzdeCj4wHY465KTIjUSaGR2dFvvM9_ieNzMAldt0&usqp=CAU"
+              alt="img - 4 ML"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
@@ -50,8 +50,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://lastmomenttuitions.com/wp-content/uploads/2021/05/Data-Structure-and-Algorithm.jpg"
-              alt="img - 5"
+              src="https://etimg.etb2bimg.com/photo/100062546.cms"
+              alt="img - 5 AI"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
@@ -60,8 +60,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://bsmedia.business-standard.com/_media/bs/img/article/2024-04/01/thumb/featurecrop/1200X900/1711910924-8793.jpg"
-              alt="img - 6"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdVBVjI-bxk-44kjraXaywABxOcJQnpl5eGocfpJBVg&s"
+              alt="img - 6 AI"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
@@ -70,8 +70,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190902124355/ruby-programming-language.png"
-              alt="img - 7"
+              src="https://qph.cf2.quoracdn.net/main-thumb-2241551221-200-jdqdjkagqcvhjjenzjjprewktdfffoxj.jpeg"
+              alt="img - 7 ML"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
@@ -80,8 +80,8 @@ const FullStackCard = () => {
             class="absolute top-0 left-0 w-full h-full origin-center [transform-style:preserve-3d] [transform:rotateY(calc(var(--i)*45deg))_translateZ(350px)]"
           >
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLNBcVhsqOUAQ8IHZNvUEKLSJE56Pb0L6f80GN4Cnbjba2wrAnAEA-_HoLyCX6wABx4u0&usqp=CAU"
-              alt="img - 8"
+              src="https://www.pulse.in/wp-content/uploads/2021/09/ai.jpg"
+              alt="img - 8 AI"
               class="absolute top-0 left-0 w-full h-full rounded-full object-cover [transition:2s] hover:-translate-y-[50px] hover:scale-[1.3]"
             />
           </span>
