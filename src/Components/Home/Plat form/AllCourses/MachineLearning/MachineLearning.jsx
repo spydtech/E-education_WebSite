abe886d99651e530ff7afd6e22948752e6591b37
Ashwini <ashwini.swe@gmail.com>
2024-05-10 19:43:58 +0530

    Updated code

--- src/Components/Home/Plat form/AllCourses/MachineLearning/MachineLearning.jsx
+++ src/Components/Home/Plat form/AllCourses/MachineLearning/MachineLearning.jsx
@@ -3,12 +3,14 @@ import Navbar from '../../../../Navbar'
 import Footer from '../../../footer/Footer'
 import MachineLearningHero from './MachineLearningHero'
 import MachineLearningCard from './MachineLearningCard'
+import MachineLearningNavigation from './MachineLearningAbout/MachineLearningNavigation'
 const MachineLearning = () => {
   return (
     <>
     <Navbar />
     <MachineLearningHero />
     <MachineLearningCard />
+    <MachineLearningNavigation />
     <Footer />
     </>
   )
