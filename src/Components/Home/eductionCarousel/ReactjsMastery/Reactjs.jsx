a8c59ee1425bec6ea5c171a62ea6ec417eb2e9ff
Ashwini <ashwini.swe@gmail.com>
2024-05-13 17:31:34 +0530

    working on Reactjs Page

--- src/Components/Home/eductionCarousel/ReactjsMastery/Reactjs.jsx
+++ src/Components/Home/eductionCarousel/ReactjsMastery/Reactjs.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from '../../../Navbar'
 import ReactJsHero from './ReactJsHero'
 import ReactjsCard from './ReactjsCard'
+import FeatureOfReactJs from './FeatureOfReactJs'
+import Footer from '../../../Home/footer/Footer'
 const Reactjs = () => {
   return (
     <>
     <Navbar />
     <ReactJsHero />
     <ReactjsCard />
+    <FeatureOfReactJs />
+    <Footer />
     </>
   )
 }
