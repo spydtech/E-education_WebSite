ce649086c709644e5b38f37cecf663f4e9696872
Ashwini <ashwini.swe@gmail.com>
2024-06-01 14:54:43 +0530

    add navbar

---
+++ src/Components/Explore.jsx
@@ -0,0 +1,285 @@
+import React from 'react'
+
+function Explore() {
+  return (
+    <div>
+ <h1 className="text-xl   bg-gray-100  border-l-8 border-green-500 rounded-md px-3 py-2">Explore Here Courses List</h1>
+ <div className = "flex flex-col md:flex-row gap-5 pt-5 p-5">
+        <div className="">
+            <h1 className="shyam cursor-pointer relative font-extrabold uppercase transition-colors duration-400 ease-cubic">Basic Course</h1>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/fullStack-Web-Development"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Web Development
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/software-testing/unit-testing"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Unit Testing
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/php"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Basic Php
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/java"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+       Basic Java
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/wordpress"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Wordpress
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/javaScript"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Basic JavaScript
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/c_c++"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Basic C/C++
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/ui_ux_design"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        UI/UX Design
+       
+    </a>
+    
+            </div>
+            
+           
+        </div>
+        <div class="flex h-48 items-center">
+  <div class="h-full border-l-2 border-[#64748b]"></div>
+</div>
+
+        <div>
+            <h1 className= "shyam cursor-pointer relative font-extrabold uppercase transition-colors duration-400 ease-cubic">Advanced Course</h1>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/mean-Stack-Developer"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        MERN Stack
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/fullStack-Java-Development"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Advance Java
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/advance-php"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Advance Php
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/data_Science/machine-Learning"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+       Machine Learning with AI
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/fullStack-Devops"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+       DevOps Mastery
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/cloud-computing"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Cloud Computing with AWS
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/Cybersecurity/cybersecurity_essentials"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Cybersecurity Advanced Practices
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/blockchain"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Blockchain Development
+
+       
+    </a>
+    
+            </div>
+           
+        </div>
+        <div class="flex h-48 items-center">
+  <div class="h-full border-l-2 border-[#64748b]"></div>
+</div>
+
+        <div>
+            <h1 className="shyam cursor-pointer relative font-extrabold uppercase transition-colors duration-400 ease-cubic">Premium Course</h1>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/fullStack-Web-Development"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Advanced Full Stack Web Development
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/software_testing"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Comprehensive Software Testing
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/advancedPhpProgramming"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Advanced PHP Programming
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/fullStack-Java-Development"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+       Java Programming Mastery
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/masteringWordPressDevelopment"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Mastering WordPress Development
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStack_WebDevelopment/full-stack-javascript"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        JavaScript for Advanced Developers
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/data_Science"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Advanced Data Science Techniques
+
+       
+    </a>
+    
+            </div>
+            <div className = "pt-6 text-xs">
+            <a href="/fullStackMobileAppDevelopment"
+        class="bg-gray-100  text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12">
+        Full Stack Mobile App Development
+
+       
+    </a>
+    
+            </div>
+           
+
+
+            <style jsx>{`
+  .shyam {
+    font-size: 18px;
+    color: black;
+    font-family: inherit;
+    border: none;
+    background: none;
+  }
+
+  .shyam:focus,
+  .shyam:hover {
+    color: #0369a1;
+  }
+
+  .shyam:focus::after,
+  .shyam:hover::after {
+    width: 100%;
+    left: 0%;
+  }
+
+  .shyam::after {
+    content: "";
+    pointer-events: none;
+    bottom: -2px;
+    left: 50%;
+    position: absolute;
+    width: 0%;
+    height: 2px;
+    background-color: #0369a1;
+    transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+    transition-duration: 400ms;
+    transition-property: width, left;
+  }
+`}</style>
+        </div>
+       
+        </div>
+    </div>
+  )
+}
+
+export default Explore
