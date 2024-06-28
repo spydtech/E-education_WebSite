6fac2e01131a2aa2fc92ad00c4559876b3ed499e
raghavendrak977 <kraghavendra944@gmail.com>
2024-06-28 12:58:59 +0530

    changes made

--- src/Components/trainee/Bar.jsx
+++ src/Components/trainee/Bar.jsx
@@ -1,39 +1,3 @@
-// import React, { useState, useEffect } from "react";
-
-// const CountdownTimer = () => {
-//   const initialSeconds = 60; // Set initial countdown time here
-//   const [seconds, setSeconds] = useState(initialSeconds);
-
-//   useEffect(() => {
-//     if (seconds > 0) {
-//       const timer = setInterval(() => {
-//         setSeconds((prevSeconds) => prevSeconds - 1);
-//       }, 1000);
-
-//       return () => clearInterval(timer);
-//     }
-//   }, [seconds]);
-
-//   const calculateDegree = () => (seconds / initialSeconds) * 360;
-
-//   return (
-//     <div className="container flex justify-center items-center h-screen">
-//       <div className="relative flex justify-center items-center w-96 h-96">
-//         <div
-//           className="absolute w-full h-full rounded-full"
-//           style={{
-//             background: `conic-gradient(red ${calculateDegree()}deg, white 0deg)`,
-//           }}
-//         ></div>
-//         <div className="absolute flex justify-center items-center w-40 h-40 bg-white rounded-full">
-//           <span className="text-2xl font-bold">{seconds}s</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CountdownTimer;
 import React, { useState, useEffect } from "react";
 
 const CountdownTimer = () => {
@@ -53,8 +17,8 @@ const CountdownTimer = () => {
   const calculateDegree = () => (seconds / initialSeconds) * 360;
 
   const getGradientColor = () => {
-    if (seconds > 30) return "green";
-    if (seconds > 10) return "yellow";
+    if (seconds > 30) return "#007f5f";
+    if (seconds > 15) return "yellow";
     return "red";
   };
 
