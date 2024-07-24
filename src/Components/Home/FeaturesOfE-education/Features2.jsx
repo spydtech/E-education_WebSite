d0691ef432ab636cfda10599af3b61e37254cd33
premchand65 <prem.pp65@gmail.com>
2024-07-24 22:09:30 +0530

    features of e eduction updated

--- src/Components/Home/FeaturesOfE-education/Features2.jsx
+++ src/Components/Home/FeaturesOfE-education/Features2.jsx
@@ -1,51 +1,191 @@
-import React from 'react';
+import React from 'react'
 
-const Features2 = () => {
+function Features2() {
   return (
     <div className="">
-      <h1 className="text-4xl font-lora text-center py-10">
-        Start your <span className='bg-gradient-to-r bg-clip-text from-[#f6ac14] to-[#0098f1] text-transparent'>Learning</span> journey
-      </h1>
-      <div className="flex flex-col sm:flex-row flex-wrap gap-5 justify-center items-center">
-        <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
-          <img src="https://s3-alpha-sig.figma.com/img/2fd8/f063/24ac1a4412736b72859c85570a113ee7?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Zn0yCHZ~rqJaIb6AmF4x~C8U-XLbakqGM9KJb57fEa8I-RgcohrPSVBrf050Q1w3b6rODd7C20NhRIQAPSF~vq0rB~5ODvOqKM8B6kOPCGE0hKtkl4N8-R9fQzNPm1Fcm-NiN6joTtQQliSUAwTcVDghv-JAo0C~h1vEA9IgKOhs4sM2w7gtqCmoJu9~45YU0kdEcnK7kFLkku7qmJHQB0Zt228P-CySWBFkkBsEkcdux~owZ6UDIBiCPG9n0jDV0szY6UMHb8QbEw4e9aPNCFzrwIns6X4OBaKeCquGyVfxpOxoKWOrteuohSqZAk0dJPhzFB6En18pnfvw50OcnQ__" alt="Explore" className="w-full h-52 object-cover" />
-          <div className="p-4">
-            <h1 className="text-2xl font-semibold mt-4">Explore</h1>
-            <p className="mt-2">
-              Browse our curated selection of courses spanning various disciplines and industries.
-            </p>
-          </div>
+    <h1 className="text-4xl font-lora text-center py-10">
+      Start your <span className='bg-gradient-to-r bg-clip-text from-[#f6ac14] to-[#0098f1] text-transparent'>Learning</span> journey
+    </h1>
+    <div className="flex flex-col sm:flex-row flex-wrap gap-5 justify-center items-center">
+      <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
+        <img src="https://s3-alpha-sig.figma.com/img/2fd8/f063/24ac1a4412736b72859c85570a113ee7?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Zn0yCHZ~rqJaIb6AmF4x~C8U-XLbakqGM9KJb57fEa8I-RgcohrPSVBrf050Q1w3b6rODd7C20NhRIQAPSF~vq0rB~5ODvOqKM8B6kOPCGE0hKtkl4N8-R9fQzNPm1Fcm-NiN6joTtQQliSUAwTcVDghv-JAo0C~h1vEA9IgKOhs4sM2w7gtqCmoJu9~45YU0kdEcnK7kFLkku7qmJHQB0Zt228P-CySWBFkkBsEkcdux~owZ6UDIBiCPG9n0jDV0szY6UMHb8QbEw4e9aPNCFzrwIns6X4OBaKeCquGyVfxpOxoKWOrteuohSqZAk0dJPhzFB6En18pnfvw50OcnQ__" alt="Explore" className="w-full h-52 object-cover" />
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold mt-4">Explore</h1>
+          <p className="mt-2">
+            Browse our curated selection of courses spanning various disciplines and industries.
+          </p>
         </div>
-        <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
-          <img src="https://s3-alpha-sig.figma.com/img/bb4a/6c2d/a6c6e5e8de393e75fea8a01b5b7853e2?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BSHykgKlLvSzS0S-bVcEOPH4kOEiBQaGxHsIoOvop5bNIct3k9J72wqoXsJsbVQa3b7wRzO4QvjTe5BYsDMmrF3PEXtcdqjETEcyO5GuGDcIFgRUlqD6b2WdDzbu2wiuyOc3skWQeQrDWu0O1fXkYoaW7MuuZzJ5CgfqKZd174Ool4o6UyZFfwoqsA0fUeq-I5Vx8Gjbas0DtWBoodm~cWkQm5g8jhq84r~3FoMD4idxiUjCnDoGHvOMlJniGzvhsJzviYYYZmanAPyacOaiyX0V3udmunzcS0Y304OswgEyrJzibulQqiJGzLJV26Xi920qeIBrIbEDbk7JqbI3aw__" alt="Enroll" className="w-full h-52 object-cover" />
-          <div className="p-4">
-            <h1 className="text-2xl font-semibold mt-4">Enroll</h1>
-            <p className="mt-2">
-              Choose the courses that align with your goals and enroll with just a few clicks.
-            </p>
-          </div>
+      </div>
+      <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
+        <img src="https://s3-alpha-sig.figma.com/img/bb4a/6c2d/a6c6e5e8de393e75fea8a01b5b7853e2?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BSHykgKlLvSzS0S-bVcEOPH4kOEiBQaGxHsIoOvop5bNIct3k9J72wqoXsJsbVQa3b7wRzO4QvjTe5BYsDMmrF3PEXtcdqjETEcyO5GuGDcIFgRUlqD6b2WdDzbu2wiuyOc3skWQeQrDWu0O1fXkYoaW7MuuZzJ5CgfqKZd174Ool4o6UyZFfwoqsA0fUeq-I5Vx8Gjbas0DtWBoodm~cWkQm5g8jhq84r~3FoMD4idxiUjCnDoGHvOMlJniGzvhsJzviYYYZmanAPyacOaiyX0V3udmunzcS0Y304OswgEyrJzibulQqiJGzLJV26Xi920qeIBrIbEDbk7JqbI3aw__" alt="Enroll" className="w-full h-52 object-cover" />
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold mt-4">Enroll</h1>
+          <p className="mt-2">
+            Choose the courses that align with your goals and enroll with just a few clicks.
+          </p>
         </div>
-        <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
-          <img src="https://s3-alpha-sig.figma.com/img/0e89/8dfb/6ce29e35b555422c56a2b24682457cd5?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jGDHNSwLWERG-MAjC1T1TgLrIjOzSsb8f0JfhERQWozT6Ly1QkIOG~0M4acp3oKeuot7VbsdsDqD-yS8uH4RiWPoeOsiFQLDevLm-nkmZ8u4Ajb5eVaC-BYGjnvICW~aL4aON~IAQ0C~v7YYUyaNXLaN0xIFG89jZ~YfYkQpPqYkGj5pDz1OQmdrZayPviboYygygVB6TfEe0WZqE1ESp1JGRGOypCEBBoozVhVvL8niRuQ4d9cB1XhsdXuCW3C~NPaVa~Wy3n8T0vfjRIiMAyUB2eJNS16iG4tgoaUxhBurCHBEjf10qgZ568JkPUFTLxJq5M-8YgyPTZdLDxAOZQ__" alt="Learn" className="w-full h-52 object-cover" />
-          <div className="p-4">
-            <h1 className="text-2xl font-semibold mt-4">Learn</h1>
-            <p className="mt-2">
-              Dive into engaging learning materials, interact with instructors, and progress at your own pace.
-            </p>
-          </div>
+      </div>
+      <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
+        <img src="https://s3-alpha-sig.figma.com/img/0e89/8dfb/6ce29e35b555422c56a2b24682457cd5?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jGDHNSwLWERG-MAjC1T1TgLrIjOzSsb8f0JfhERQWozT6Ly1QkIOG~0M4acp3oKeuot7VbsdsDqD-yS8uH4RiWPoeOsiFQLDevLm-nkmZ8u4Ajb5eVaC-BYGjnvICW~aL4aON~IAQ0C~v7YYUyaNXLaN0xIFG89jZ~YfYkQpPqYkGj5pDz1OQmdrZayPviboYygygVB6TfEe0WZqE1ESp1JGRGOypCEBBoozVhVvL8niRuQ4d9cB1XhsdXuCW3C~NPaVa~Wy3n8T0vfjRIiMAyUB2eJNS16iG4tgoaUxhBurCHBEjf10qgZ568JkPUFTLxJq5M-8YgyPTZdLDxAOZQ__" alt="Learn" className="w-full h-52 object-cover" />
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold mt-4">Learn</h1>
+          <p className="mt-2">
+            Dive into engaging learning materials, interact with instructors, and progress at your own pace.
+          </p>
         </div>
-        <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
-          <img src="https://s3-alpha-sig.figma.com/img/9523/1cf7/27e9bdbec2b681ae490f7b697a716983?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ml5K7a5UGkX82v7uVMJEb-egCZ1C~lFmd-0FAh8VPEFwLNPlEIYAsFwNT5xhz9DOsS6HVYuvGY31QYfNPLAlpk4J1j4SJ23GJJvJgRzas8zEopPlXpXCSlrJWlwd66IkHgn6uG-byVwmxuKq1PVBmihKLaTFf04ym0QgTcssSyYDYW9OS3tTc5Sab2qX1iEd-TBW~2A8ttVbfApumVeokYeQ8p9gOBQ77XUtFV9vswvv8RiLCtHo26KFsQcSvij5zEKK0VTUD3q7X9fsRdywo0A9mcLdxwRFGTsLix-a10UauF531sqn6R8fIi2LNhCHAl77e6e-8W1AKndvwvXBlQ__" alt="Achieve" className="w-full h-52 object-cover" />
-          <div className="p-4">
-            <h1 className="text-2xl font-semibold mt-4">Achieve</h1>
-            <p className="mt-2">
-              Master new skills, earn certifications, and achieve your personal and professional goals.
-            </p>
-          </div>
+      </div>
+      <div className="w-[293px] h-[400px] bg-[#0098f1] text-white flex flex-col items-center">
+        <img src="https://s3-alpha-sig.figma.com/img/9523/1cf7/27e9bdbec2b681ae490f7b697a716983?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ml5K7a5UGkX82v7uVMJEb-egCZ1C~lFmd-0FAh8VPEFwLNPlEIYAsFwNT5xhz9DOsS6HVYuvGY31QYfNPLAlpk4J1j4SJ23GJJvJgRzas8zEopPlXpXCSlrJWlwd66IkHgn6uG-byVwmxuKq1PVBmihKLaTFf04ym0QgTcssSyYDYW9OS3tTc5Sab2qX1iEd-TBW~2A8ttVbfApumVeokYeQ8p9gOBQ77XUtFV9vswvv8RiLCtHo26KFsQcSvij5zEKK0VTUD3q7X9fsRdywo0A9mcLdxwRFGTsLix-a10UauF531sqn6R8fIi2LNhCHAl77e6e-8W1AKndvwvXBlQ__" alt="Achieve" className="w-full h-52 object-cover" />
+        <div className="p-4">
+          <h1 className="text-2xl font-semibold mt-4">Achieve</h1>
+          <p className="mt-2">
+            Master new skills, earn certifications, and achieve your personal and professional goals.
+          </p>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+  )
+}
 
-export default Features2;
+export default Features2
+
+
+// import React from 'react'
+
+// function Features2() {
+//   return (
+//     <div>
+//         <div className='flex flex-col md:flex-row gap-10 py-10'>
+//         <div class="card1">
+//   <div class="card-image1 ">
+//     <img src='https://www.shutterstock.com/image-vector/creative-thinking-process-study-activities-600nw-215369221.jpg' 
+//     className='rounded-lg h-[200px] w-[300px]'/>
+//   </div>
+//   <p class="card-title1">Explore</p>
+//   <p class="card-body">
+//   Browse our curated selection of courses spanning various disciplines and industries.
+//   </p>
+//  </div>
+// {/* 2nd card */}
+// <div class="card1">
+//   <div class="card-image1">
+//     <img 
+//     src='https://cdn.pixabay.com/photo/2018/03/18/11/58/recruit-3236547_640.jpg'
+//     className='rounded-lg h-[200px] w-[300px]'/>
+//   </div>
+//   <p class="card-title1">Enroll</p>
+//   <p class="card-body">
+//   Choose the courses that align with your goals and enroll with just a few clicks.
+//   </p>
+//   </div>
+// {/* 3rd card */}
+// <div class="card1">
+//   <div class="card-image1">
+//     <img 
+//     src='https://img.freepik.com/free-vector/soft-skills-isometric-symbolic-composition-with-woman-employee-improving-personal-development-achieve-business-goals_1284-59854.jpg?w=740&t=st=1714053350~exp=1714053950~hmac=433591cc3bb82c289b19a3f7f62d64aa6e97917633054848d4463fb9b29d322d'
+//     className='rounded-lg h-[200px] w-[300px]'
+//     />
+//   </div>
+//   <p class="card-title1">Learn</p>
+//   <p class="card-body">
+//   Dive into engaging learning materials, interact with instructors, and progress at your own pace.
+//   </p>
+//  </div>
+
+// {/* 4th card */}
+// <div class="card1">
+//   <div class="card-image1">
+//   <img 
+//     src='https://img.freepik.com/premium-vector/outstanding-students-bring-trophies-awards_18660-767.jpg?w=740'
+//     className='rounded-lg h-[200px] w-[300px]'
+//     />
+//   </div>
+//   <p class="card-title1">Achieve</p>
+//   <p class="card-body">
+//   Master new skills, earn certifications, and achieve your personal and professional goals.
+//   </p>
+//  </div>
+
+//         </div>
+       
+//        <style jsx>{`
+//        .card1 {
+//         padding: 20px;
+//         width: 330px;
+//         min-height: 370px;
+//         border-radius: 20px;
+//         background: #e8e8e8;
+//         box-shadow: 5px 5px 6px #dadada,
+//                      -5px -5px 6px #f6f6f6;
+//         transition: 0.4s;
+//       }
+      
+//       .card:hover {
+//         translate: 0 -10px;
+//       }
+      
+//       .card-title1 {
+//         font-size: 18px;
+//         font-weight: 600;
+//         color: #2e54a7;
+//         margin: 15px 0 0 10px;
+//       }
+      
+//       .card-image1 {
+//         min-height: 170px;
+//         background-color: #c9c9c9;
+//         border-radius: 15px;
+//         box-shadow: inset 8px 8px 10px #c3c3c3,
+//                   inset -8px -8px 10px #cfcfcf;
+//       }
+      
+//       .card-body {
+//         margin: 13px 0 0 10px;
+//         color: rgb(31, 31, 31);
+//         font-size: 15px;
+//       }
+      
+//       .footer {
+//         float: right;
+//         margin: 28px 0 0 18px;
+//         font-size: 13px;
+//         color: #636363;
+//       }
+      
+//       .by-name {
+//         font-weight: 700;
+//       }
+       
+//        `}</style>
+//        <div>
+//        <h1 class="text-2xl md:text-3xl pl-2 my-2 border-l-4  font  border-teal-400  dark:text-gray-200">
+//        Join the E-education Revolution
+// </h1>
+
+// <div class="sm:flex items-center max-w-screen-xl">
+//     <div class="sm:w-1/2 p-10">
+//         <div class="image object-center text-center">
+//             <img src="https://i.imgur.com/WbQnbas.png" />
+//         </div>
+//     </div>
+//     <div class="sm:w-1/2 p-5">
+//         <div class="text">
+//             <span class="text-gray-500 border-b-2 border-indigo-600 uppercase">About us</span>
+//             <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">Join <span class="text-indigo-600">E-education</span>
+//             </h2>
+//             <p class="text-gray-700 pb-5">
+//             Experience the power of E-education and take control of your learning journey. 
+//             Start today and embark on a path of continuous growth and success.
+//             </p>
+//             <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border-2 border-indigo-600
+//         before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-indigo-500 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700">Join</button>
+//         </div>
+//     </div>
+// </div>
+//        </div>
+//     </div>
+//   )
+// }
+
+// export default Features2
