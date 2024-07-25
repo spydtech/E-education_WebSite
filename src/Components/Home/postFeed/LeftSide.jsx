0e404423a93055e0be36344214767ed0af5aeb54
Ashwini Kumari <ashwini.swe@gmail.com>
2024-07-25 23:55:14 +0530

    working on post section

---
+++ src/Components/Home/postFeed/LeftSide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftSide = () => {
+  return (
+    <div className='border border-blue-500 w-[241px] h-[100%]'>LeftSide</div>
+  )
+}
+
+export default LeftSide
---
+++ src/Components/Home/postFeed/RightSide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rightside = () => {
+  return (
+    <div className='border border-blue-500 w-[333px] h-[100%]'>Rightside</div>
+  )
+}
+
+export default Rightside
