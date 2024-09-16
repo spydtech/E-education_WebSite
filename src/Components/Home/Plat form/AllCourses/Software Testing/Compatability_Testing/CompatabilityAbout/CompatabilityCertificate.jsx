0ae4fd9a8147b891269b7c3bc98047df70885d4f
nikhilraj021 <dnraj2001@gmail.com>
2024-09-16 08:09:05 +0530

    updated certificate

--- src/Components/Home/Plat form/AllCourses/Software Testing/Compatability_Testing/CompatabilityAbout/CompatabilityCertificate.jsx
+++ src/Components/Home/Plat form/AllCourses/Software Testing/Compatability_Testing/CompatabilityAbout/CompatabilityCertificate.jsx
@@ -4,23 +4,23 @@ import certificate from "../../../../../../../assetss/certficate.png";
 function CompatabilityCertificate() {
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 p-4 justify-center items-center md:px-10 md:gap-8">
-    {/* Certificate Image */}
-    <div className="flex justify-center items-center">
-      <img src={certificate} alt="certificate" className="" />
-    </div>
+      {/* Certificate Image */}
+      <div className="flex justify-center items-center">
+        <img src={certificate} alt="certificate" className="" />
+      </div>
 
-    {/* Text Content */}
-    <div className="space-y-3 text-center md:text-left">
-      <p className="md:text-2xl font-bold text-[#0098F1]">
-        Achieve Credentials and Recognition
-      </p>
-      <p className="md:text-xl text-gray-500">
-        Successfully complete all course modules to obtain an Ethical Hacking
-        Fundamentals certification, greatly enhancing career prospects and
-        skill development.
-      </p>
+      {/* Text Content */}
+      <div className="space-y-3 text-center md:text-left">
+        <p className="md:text-2xl font-bold text-[#0098F1]">
+          Achieve Credentials and Recognition
+        </p>
+        <p className="md:text-xl text-gray-500">
+          Successfully complete all course modules to obtain an Ethical Hacking
+          Fundamentals certification, greatly enhancing career prospects and
+          skill development.
+        </p>
+      </div>
     </div>
-  </div>
   );
 }
 
