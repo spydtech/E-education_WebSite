feab084654ad440423c541fb96669e0ea16e66bb
Ashwini Kumari <ashwini.swe@gmail.com>
2024-09-17 23:11:36 +0530

    added Automation testing page

--- src/Components/Home/Plat form/AllCourses/Software Testing/Automation_Testing/AutomationTestingAbout/AutomationTestingAbout.jsx
+++ src/Components/Home/Plat form/AllCourses/Software Testing/Automation_Testing/AutomationTestingAbout/AutomationTestingAbout.jsx
@@ -4,44 +4,45 @@ import programmer from "../../../../../../../assets/dataAnalatics/programmer.jpg
 import assistance from "../../../../../../../assets/dataAnalatics/assistance.jpg";
 
 const AutomationTestingAbout = () => {
-  const softwareTestingInfo = [
+  const ethicalHackingInfo = [
     {
       title: "Career Assistance",
       description:
         "Stay ahead with access to one-on-one career counseling sessions and mock interviews with industry professionals.",
-      imgSrc:"https://img.freepik.com/free-photo/rear-view-programmer-working-all-night-long_1098-18697.jpg?ga=GA1.2.481732087.1715859514&semt=ais_hybrid",
+      imgSrc: "https://img.freepik.com/premium-vector/software-development-programming-language-coding-isometric-pc-computer-with-digital-application-white-background_284092-318.jpg?w=740",
       imgAlt: "career-assistance",
     },
     {
       title: "Student Support",
       description:
-        'Access student support via support@datasciencecourse.com or through the "ask us?" option on our learning platform for urgent queries.',
-      imgSrc:"https://img.freepik.com/free-photo/modern-equipped-computer-lab_23-2149241221.jpg?ga=GA1.1.481732087.1715859514&semt=ais_hybrid",
+        'Access student support via support@automationtesting.com or through the "ask us?" option on our learning platform for urgent queries.',
+      imgSrc: "https://img.freepik.com/free-vector/hacker-activity-isometric-composition_1284-21475.jpg?t=st=1716550896~exp=1716554496~hmac=5802aeca8133c034a5da30c58e2e1eb86602a116a2c98b917ed68ea329193008&w=740",
       imgAlt: "student-support",
     },
   ];
 
   return (
-    <div className="mx-auto h-auto w-full mb-20 px-4 md:px-10 bg-white">
-      <div className="py-2 my-10">
-        <p className="text-2xl text-center font-bold text-[#f6ac14]">
-          Discover the   Unit Testing Program
+    <div className="px-3 text-center">
+      <div className="">
+        <p className="md:text-2xl font-bold text-[#0098F1]">
+          Discover the Automatin Testing Program
         </p>
       </div>
 
-      <div className="flex flex-wrap justify-center items-center pb-6">
-        {softwareTestingInfo.map((item, index) => (
-          <div key={index} className="w-full  md:w-1/2 lg:w-1/3 px-4 mb-8">
-            <div className="bg-white h-[400px] rounded-lg shadow-lg overflow-hidden">
-              <img
-                className="w-full h-52 object-cover object-center"
-                src={item.imgSrc}
-                alt={item.imgAlt}
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-bold mb-2">{item.title}</h2>
-                <p className="text-gray-700">{item.description}</p>
-              </div>
+      <div className="md:flex justify-around">
+        {ethicalHackingInfo.map((item, index) => (
+          <div
+            key={index}
+            className=" md:w-2/5 md:h-96 bg-white rounded-lg shadow-lg p-3 my-4"
+          >
+            <img
+              className="md:w-full md:h-64 rounded-md"
+              src={item.imgSrc}
+              alt={item.imgAlt}
+            />
+            <div className="my-3">
+              <h2 className="md:text-xl font-bold">{item.title}</h2>
+              <p className="text-gray-700">{item.description}</p>
             </div>
           </div>
         ))}
