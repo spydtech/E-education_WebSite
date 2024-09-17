c4b3d3682a81ab1923f4aba2c7430de892b4d8d6
Ashwini Kumari <ashwini.swe@gmail.com>
2024-09-17 23:50:31 +0530

    security testing page added

--- src/Components/Home/Plat form/AllCourses/Software Testing/Security_Testing/SecurityTestingAbout/SecurityTestingKeyHighights.jsx
+++ src/Components/Home/Plat form/AllCourses/Software Testing/Security_Testing/SecurityTestingAbout/SecurityTestingKeyHighights.jsx
@@ -4,65 +4,11 @@ function SecurityTestingKeyHighlights() {
   const [selectedItem, setSelectedItem] = useState(0);
 
   const data = [
-    {
-      label: "Overview",
-      items: [
-        "Advanced modules with Generative AI fusion",
-        "E-education certificate via upGrad collaboration",
-        "Access to live training sessions by Meta Blueprint trainers",
-        "15+ Case Studies, Live projects",
-        "Earn E-Education Executive Alumni Status",
-        "90+ Tool Video Library",
-        "Industry mentor-led fortnightly coaching",
-        "Tailored resume feedback",
-        "Mock Interview by managers",
-        "Explore ChatGPT in digital marketing tutorials"
-      ]
-    },
-    {
-      label: "Key Concepts",
-      items: [
-        "Generative AI in digital marketing",
-        "SEO",
-        "Search and Display advertising",
-        "Social and Display advertising",
-        "Content Marketing",
-        "Branding",
-        "Web Analytics"
-      ]
-    },
-    {
-      label: "Applications",
-      items: [
-        "Digital Marketing Manager",
-        "SEM Manager",
-        "SEO Manager",
-        "Social Media and Content Manager",
-        "Associate",
-        "Consultant",
-        "Business Analyst",
-        "(Sr.)Associate-Digital Marketing"
-      ]
-    },
-    {
-      label: "Skills Developed",
-      items: [
-        "List of target audience goes here...",
-        "Explore 2 tracks & 5 specializations for diverse learning.",
-        "Experience seamless integration with Gen AI modules.",
-        "Attain E-education Executive Alumni Status for exclusive benefits.",
-        "Access EMI at RS. 2,879/month for greater flexibility.",
-        "A voucher worth $50 for Meta's 'Digital Marketing Associate' certificate exam, free of cost."
-      ]
-    },
-    {
-      label: "Target Audience",
-      items: [
-        "Utilize ChatGPT to assist in creative unique content, plan strategies, build calendars, and more",
-        "Generative SEO-optimized content with ChatGPT",
-        "Gain insights into your social media analytics by using ChatGPT"
-      ]
-    }
+    { label: "Overview" },
+    { label: "Key Concepts" },
+    { label: "Applications" },
+    { label: "Skills Developed" },
+    { label: "Target Audience" },
   ];
 
   const handleClick = (index) => {
@@ -70,54 +16,133 @@ function SecurityTestingKeyHighlights() {
   };
 
   const renderContent = () => {
-    const selectedData = data[selectedItem];
-    return (
-      <ul className="space-y-3 pt-4 font-normal text-[16px] list-disc">
-        <p className="text-2xl font-medium text-[#f6ac14] py-4">
-          {selectedData.label}
-        </p>
-        {selectedData.items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    );
+    switch (selectedItem) {
+      case 0:
+        return (
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">Overview</p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+            <li>Advanced modules with Generative AI fusion</li>
+              <li>E-education certificate via upGrad collaboration</li>
+              <li>
+                Access to live training sessions by Meta Blueprint trainers
+              </li>
+              <li>15+ Case Studies, Live projects</li>
+              <li>Earn E-Eduction Executive Alumni Status</li>
+              <li>90+ Tool Video Library</li>
+              <li>Industry mentor-led fortnightly coaching</li>
+              <li>Tailored resume feedback</li>
+              <li>Mock Interview by managers</li>
+              <li>Explore ChatGPT in digital marketing tutorials</li>
+            </ul>
+          </div>
+        );
+      case 1:
+        return (
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">Key Concepts</p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+            <li>Generative AI in digital marketing</li>
+              <li>SEO</li>
+              <li>Search and Display advertising</li>
+              <li>Social and Display advertising</li>
+              <li>Content Marketing</li>
+              <li>Branding</li>
+              <li>Web Analytics</li>
+            </ul>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">Applications</p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+            <li>Digital Marketing Manager</li>
+              <li>SEM Manager</li>
+              <li>SEO Manager</li>
+              <li>Social Media and Content Manager</li>
+              <li>Associate</li>
+              <li>Consultant</li>
+              <li>Business Analyst</li>
+              <li>(Sr.)Associate-Digital Marketing</li>
+            </ul>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">
+              Skills Developed
+            </p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+            <li>List of target audience goes here...</li>
+              <li>
+                Explore 2 tracks & 5 specializations for diverse learning.
+              </li>
+              <li>Experience seamless integration with Gen AI modules.</li>
+              <li>
+                Attain E-education Executive Alumni Status for exclusive
+                benefits.
+              </li>
+              <li>Access EMI at RS. 2,879/month for greater flexibility.</li>
+              <li>
+                A voucher worth $50 for Meta's "Digital Marketing Associate"
+                certificate exam, free of cost.
+              </li>
+            </ul>
+          </div>
+        );
+      case 4:
+        return (
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">
+              Target Audience
+            </p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+            <li>
+                Utilise ChatGPT to assist in creative unique content,plan
+                strategies,build,calenders,and more
+              </li>
+              <li>Generative SEO-optimised content with chatGPT</li>
+              <li>
+                Gain insights into your social media analytics by using chatGPT
+              </li>
+            </ul>
+          </div>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
-    <div className="px-20 mb-20 bg-white">
-      <div className="pt-4">
-        <p className="font-bold underline-offset-2 underline text-xl p-2 text-[#f6ac14]">
-          E-education Course Highlights
-        </p>
+    <div className="px-3 md:mb-4">
+    <div className="md:flex justify-around  ">
+      <div className="bg-[#0098F1] text-white  pb-5 md:w-2/5">
+        {renderContent()}
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start justify-center">
-        <div className="w-auto h-auto md:w-[500px] md:h-[420px] mt-10 md:pl-10 pl-0 text-xl">
-          {renderContent()}
-        </div>
-        <div className="lg:pl-14">
-          <div className="pt-4">
-            <p className="sm:text-2xl font-bold text-[#f6ac14] inline text-xs">
-              What does this Course have to offer?
-            </p>
-          </div>
-          <div className="w-auto h-auto xs:w-[20px] md:w-[500px] md:h-[420px] sm:w-[250px] mt-10 md:pl-16 pl-0 items-center">
-            {data.map((item, index) => (
-              <div
-                key={index}
-                onClick={() => handleClick(index)}
-                className={`p-5 text-center hover:cursor-pointer text-xl font-medium my-6 mx-4 h-16 w-[210px] flex items-center border-2 border-opacity-15 rounded-md overflow-hidden transition-transform duration-300 transform hover:scale-105 ${
-                  selectedItem === index
-                    ? "bg-[#0098f1] text-white border-none"
-                    : ""
-                }`}
-              >
-                <p className="text-center">{item.label}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+      <div className="my-3">
+        <p className="font-bold underline-offset-2 underline text-xl text-[#0098F1]">
+          E-Education Course Highlights
+        </p>
+        {data.map((item, index) => (
+          <ul className="">
+            <li
+              key={index}
+              onClick={() => handleClick(index)}
+              className={`pt-4 hover:cursor-pointer text-xl my-2 md:h-16 md:w-[210px] border-2 rounded-md text-center  ${
+                selectedItem === index
+                  ? "bg-[#0098f1] text-white border-none"
+                  : ""
+              }`}
+            >
+              {item.label}
+            </li>
+          </ul>
+        ))}
       </div>
     </div>
+  </div>
   );
 }
 
