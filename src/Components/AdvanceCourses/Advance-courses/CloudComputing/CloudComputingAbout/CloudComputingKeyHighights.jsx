9929245f585ff7f15015b5b87062b9336e7b604a
raghavendrak977 <kraghavendra944@gmail.com>
2024-09-19 15:37:02 +0530

    cloud computing with aws done responsive

--- src/Components/AdvanceCourses/Advance-courses/CloudComputing/CloudComputingAbout/CloudComputingKeyHighights.jsx
+++ src/Components/AdvanceCourses/Advance-courses/CloudComputing/CloudComputingAbout/CloudComputingKeyHighights.jsx
@@ -19,65 +19,96 @@ function CloudComputingKeyHighlights() {
     switch (selectedItem) {
       case 0:
         return (
-          <ul className="space-y-2 pt-4 font-normal text-[16px] list-disc">
-            <p className="text-2xl font-medium text-[#f6ac14] py-4">Overview</p>
-            <li>Introduction to cloud computing and its benefits.</li>
-            <li>Understanding various cloud service models: IaaS, PaaS, SaaS.</li>
-            <li>Overview of major cloud platforms: AWS, Azure, Google Cloud.</li>
-            <li>Hands-on experience with cloud deployment and management.</li>
-            <li>Explore real-world case studies and practical applications.</li>
-          </ul>
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">Overview</p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+              <li>Advanced modules with Generative AI fusion</li>
+              <li>E-education certificate via upGrad collaboration</li>
+              <li>
+                Access to live training sessions by Meta Blueprint trainers
+              </li>
+              <li>15+ Case Studies, Live projects</li>
+              <li>Earn E-Eduction Executive Alumni Status</li>
+              <li>90+ Tool Video Library</li>
+              <li>Industry mentor-led fortnightly coaching</li>
+              <li>Tailored resume feedback</li>
+              <li>Mock Interview by managers</li>
+              <li>Explore ChatGPT in digital marketing tutorials</li>
+            </ul>
+          </div>
         );
       case 1:
         return (
-          <ul className="space-y-3 pt-4 font-normal text-[16px] list-disc">
-            <p className="text-2xl font-medium text-[#f6ac14] py-4">Key Concepts</p>
-            <li>Cloud Service Models: IaaS, PaaS, SaaS</li>
-            <li>Virtualization and Containers</li>
-            <li>Cloud Storage Solutions</li>
-            <li>Scalability and Elasticity</li>
-            <li>Cloud Security and Compliance</li>
-            <li>Disaster Recovery and Backup</li>
-            <li>Cost Management and Optimization</li>
-          </ul>
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">Key Concepts</p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+              <li>Generative AI in digital marketing</li>
+              <li>SEO</li>
+              <li>Search and Display advertising</li>
+              <li>Social and Display advertising</li>
+              <li>Content Marketing</li>
+              <li>Branding</li>
+              <li>Web Analytics</li>
+            </ul>
+          </div>
         );
       case 2:
         return (
-          <ul className="space-y-3 pt-4 font-normal text-[16px] list-disc">
-            <p className="text-2xl font-medium text-[#f6ac14] py-4">Applications</p>
-            <li>Developing and Deploying Applications on the Cloud</li>
-            <li>Managing Cloud Infrastructure</li>
-            <li>Data Analytics and Big Data Solutions</li>
-            <li>Disaster Recovery Planning</li>
-            <li>Cloud-based Business Intelligence</li>
-            <li>Cost-effective Cloud Solutions for Enterprises</li>
-            <li>Hybrid Cloud Implementations</li>
-          </ul>
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">Applications</p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+              <li>Digital Marketing Manager</li>
+              <li>SEM Manager</li>
+              <li>SEO Manager</li>
+              <li>Social Media and Content Manager</li>
+              <li>Associate</li>
+              <li>Consultant</li>
+              <li>Business Analyst</li>
+              <li>(Sr.)Associate-Digital Marketing</li>
+            </ul>
+          </div>
         );
       case 3:
         return (
-          <ul className="space-y-3 pt-4 font-normal text-[16px] list-disc">
-            <p className="text-2xl font-medium text-[#f6ac14] py-4">Skills Developed</p>
-            <li>Cloud Architecture Design and Implementation</li>
-            <li>Managing Cloud Resources and Services</li>
-            <li>Implementing Security Measures in the Cloud</li>
-            <li>Optimizing Cloud Performance and Cost</li>
-            <li>Developing Cloud-based Applications</li>
-            <li>Understanding Cloud Provider Ecosystems</li>
-            <li>Disaster Recovery and Backup Strategies</li>
-          </ul>
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">
+              Skills Developed
+            </p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+              <li>List of target audience goes here...</li>
+              <li>
+                Explore 2 tracks & 5 specializations for diverse learning.
+              </li>
+              <li>Experience seamless integration with Gen AI modules.</li>
+              <li>
+                Attain E-education Executive Alumni Status for exclusive
+                benefits.
+              </li>
+              <li>Access EMI at RS. 2,879/month for greater flexibility.</li>
+              <li>
+                A voucher worth $50 for Meta's "Digital Marketing Associate"
+                certificate exam, free of cost.
+              </li>
+            </ul>
+          </div>
         );
       case 4:
         return (
-          <ul className="space-y-3 pt-4 font-normal text-[16px] list-disc">
-            <p className="text-2xl font-medium text-[#f6ac14] py-4">Target Audience</p>
-            <li>IT Professionals and Cloud Engineers</li>
-            <li>Software Developers and DevOps Engineers</li>
-            <li>System Administrators</li>
-            <li>Data Analysts and Data Scientists</li>
-            <li>Business Leaders and IT Managers</li>
-            <li>Anyone interested in cloud technologies and their applications</li>
-          </ul>
+          <div className="p-3 space-y-2 ">
+            <p className="text-xl font-medium text-white pl-2">
+              Target Audience
+            </p>
+            <ul className="space-y-2 font-normal list-disc pl-6">
+              <li>
+                Utilise ChatGPT to assist in creative unique content,plan
+                strategies,build,calenders,and more
+              </li>
+              <li>Generative SEO-optimised content with chatGPT</li>
+              <li>
+                Gain insights into your social media analytics by using chatGPT
+              </li>
+            </ul>
+          </div>
         );
       default:
         return null;
@@ -85,32 +116,30 @@ function CloudComputingKeyHighlights() {
   };
 
   return (
-    <div className="px-10 mb-20 bg-white">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start justify-center">
-        <div className="w-auto h-auto md:w-[500px] md:h-[420px] mt-10 md:pl-10 pl-0 text-xl">
+    <div className="px-3 md:mb-4">
+      <div className="md:flex justify-around  ">
+        <div className="bg-[#0098F1] text-white  pb-5 md:w-2/5">
           {renderContent()}
         </div>
-        <div className="md:pl-10 pl-0">
-          <div className="pt-4">
-            <p className="font-bold underline-offset-2 underline text-xl  text-[#f6ac14]">
-              Cloud Computing Course Highlights
-            </p>
-          </div>
-          <div className="pt-4 lg:ml-10">
-            {data.map((item, index) => (
-              <div
+        <div className="my-3">
+          <p className="font-bold underline-offset-2 underline text-xl text-[#0098F1]">
+            E-Education Course Highlights
+          </p>
+          {data.map((item, index) => (
+            <ul className="">
+              <li
                 key={index}
                 onClick={() => handleClick(index)}
-                className={`p-5 text-center hover:cursor-pointer text-xl font-medium my-6 mx-4 h-16 w-[210px] flex items-center border-2 border-opacity-15 rounded-md overflow-hidden transition-transform duration-300 transform hover:scale-105 ${
+                className={`pt-4 hover:cursor-pointer text-xl my-2 md:h-16 md:w-[210px] border-2 rounded-md text-center  ${
                   selectedItem === index
                     ? "bg-[#0098f1] text-white border-none"
                     : ""
                 }`}
               >
-                <p className="text-center">{item.label}</p>
-              </div>
-            ))}
-          </div>
+                {item.label}
+              </li>
+            </ul>
+          ))}
         </div>
       </div>
     </div>
