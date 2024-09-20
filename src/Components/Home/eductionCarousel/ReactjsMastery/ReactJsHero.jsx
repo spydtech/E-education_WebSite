9d74f8dbe399c928c61803069a8eb281b87ec0f1
raghavendrak977 <kraghavendra944@gmail.com>
2024-09-20 17:35:49 +0530

    updated code

--- src/Components/Home/eductionCarousel/ReactjsMastery/ReactJsHero.jsx
+++ src/Components/Home/eductionCarousel/ReactjsMastery/ReactJsHero.jsx
@@ -24,22 +24,22 @@ const ReactJsHero = () => {
               Revolutionize Web Development!
             </h2>
             <h3 className="mt-4 mx-2  md:mt-4 text-md lg:text-xl text-center md:text-left  ">
-              ReactJs is declarative, component-based design simplifies UI
+              ReactJs is declarative, component based design simplifies UI
               creation for top-notch performance. Whether you're a novice or
               expert, React equips you to craft responsive, scalable web apps.
               Dive in now and redefine your development journey!
             </h3>
-            <div className="mt-10 mx-2  flex flex-col sm:flex-row justify-center md:justify-start">
+            <div className="mt-10 mx-2  flex flex-col sm:flex-row justify-center  md:justify-start">
               <button
                 onClick={handleEnroll}
-                className="w-40 ml-2 sm:w-40 p-2 sm:px-4 sm:py-4 rounded  font-semibold text-lg bg-[#0098f1] text-white "
+                className=" ml-2 sm:w-40 p-2 sm:px-4 sm:py-4 rounded  font-semibold text-lg bg-[#0098f1] text-white "
               >
                 Get started
               </button>
             </div>
           </div>
           <div className="w-full md:w-1/2 flex justify-center">
-            <FaReact className="md:w-[400px] md:h-[400px] w-80 h-80 animate-spin-slow text-[#0098f1]" />
+            <FaReact className=" md:w-[400px] md:h-[400px] w-64 h-64 animate-spin-slow text-[#0098f1]" />
           </div>
         </div>
       </div>
