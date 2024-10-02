c88cc015fafb28dd0c7760f92c9c8c3142a9119c
DineshNetha <dineshkumarnetha18@gmail.com>
2024-10-02 15:01:25 +0530

    updated code

--- src/Components/Home/Professional Certificates/Certificates.jsx
+++ src/Components/Home/Professional Certificates/Certificates.jsx
@@ -31,9 +31,9 @@ function Certificates() {
               </a>
             </li>
             <li>
-              <div class="flex items-center">  
+              <div class="flex items-center">     
                 <svg    
-                  class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                  class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"   
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
