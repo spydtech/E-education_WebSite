717b1bf7167b3650a5e37f6000218842c3da9249
raghavendrak9 <raghavendra@spydtech.com>
2024-10-08 17:30:42 +0530

    updated main-post

--- src/Components/Home/postFeed/LeftSide.jsx
+++ src/Components/Home/postFeed/LeftSide.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import mans from "../../../assetss/profile/man.png"
+import React from "react";
+import mans from "../../../assetss/profile/man.png";
 const LeftSide = () => {
   return (
-    <div className='absolute top-[32px] left-[8px] border border-blue-500 w-[270px] h-[271px] bg-[#0098F1] rounded-md'>
-    <div className=' flex flex-col items-center justify-center w-full h-full'>
-      <img src={mans} alt='Profile' className='h-[80px] w-[80px] rounded-full mb-2 border-4 border-white ' />
-      <div className='text-white font-semibold text-lg'>
-        Cameron Williamson
-      </div>
-      <div className='text-white'>
-        Cameronwilliamson@gmail.com
-      </div>
-      <hr className='border-t-2 border-white my-4 w-1/2  mx-8' />
-      <div className='gap-4 '>
-        <div className='flex justify-center items-center flex-row text-white gap-16'>
-          <div>Who viewed your profile</div>
-          <div>90</div>
+    <div className="  w-[250px] xl:w-[300px] pt-10 pb-6 p-4 h-[300px] bg-[#0098F1] rounded-md">
+      <div className=" flex flex-col items-center justify-center w-full h-full">
+        <img
+          src={mans}
+          alt="Profile"
+          className="h-[80px] w-[80px] rounded-full mb-2 border-4 border-white "
+        />
+        <div className="text-white font-semibold text-lg">
+          Cameron Williamson
         </div>
-        <div className='flex justify-between items-center flex-row text-white gap-24 mt-2'>
-          <div>Views of your post</div>
-          <div>627</div>
+        <div className="text-white">Cameronwilliamson@gmail.com</div>
+        <hr className="border-t-2 border-white my-4 w-1/2  mx-8" />
+        <div className="gap-4 ">
+          <div className="flex justify-center items-center flex-row text-white gap-16">
+            <div>Who viewed your profile</div>
+            <div>90</div>
+          </div>
+          <div className="flex justify-between items-center flex-row text-white gap-24 mt-2">
+            <div>Views of your post</div>
+            <div>627</div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default LeftSide
+export default LeftSide;
--- src/Components/Home/postFeed/MainPost.jsx
+++ src/Components/Home/postFeed/MainPost.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import LeftSide from './LeftSide'
-import Post from './Post'
-import RightSide from './RightSide'
-const MainPost = () => {
-    return (
-        <div className='flex flex-row justify-center items-center gap-44'>
-            <div>
-                <LeftSide />
-            </div>
-            <div>
-                <Post />
-            </div>
-            <div>
-                <RightSide />
-            </div>
-        </div>
-    )
-}
+// import React from "react";
+// import LeftSide from "./LeftSide";
+// import Post from "./Post";
+// import RightSide from "./RightSide";
+// const MainPost = () => {
+//   return (
+//     <div className="grid-cols-1  w-auto grid lg:grid-cols-3  gap-4 justify-between mx-auto p-2  py-2">
+//       <div className="bg-yellow-400 w-[300px] ">
+//         <LeftSide />
+//       </div>
+//       <div className=" lg:mt-0 mt-96 w-[600px] mx-4 ">
+//         <Post />
+//       </div>
+//       <div className=" bg-blue-400">
+//         <RightSide />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default MainPost
+// export default MainPost;
+import React from "react";
+import LeftSide from "./LeftSide";
+import Post from "./Post";
+import RightSide from "./RightSide";
+import Navbar from "../../Navbar";
+
+const MainPost = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="grid grid-cols-1 lg:grid-cols-4   lg:w-auto  p-2 gap-4">
+        {/* Left Side */}
+        <div className="">
+          <LeftSide />
+        </div>
+
+        {/* Middle Post Section */}
+        <div className="   lg:mx-0">
+          <Post />
+        </div>
+
+        {/* Right Side */}
+        <div className=" absolute right-3  ">
+          <RightSide />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPost;
